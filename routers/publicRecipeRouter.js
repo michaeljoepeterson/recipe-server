8168d95df28565e12e743370244644ec044b5227
Michael Peterson <michaeljoepeterson@gmail.com>
2020-03-06 08:10:21 -0700

    added get single recipe endpoint

--- routers/publicRecipeRouter.js
+++ routers/publicRecipeRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Recipe} = require('../models/recipes');
 const router = express.Router();
-
+//bulk get recipes
 router.get('/',(req,res) =>{
     let {skip,limit} = req.query;
     limit = limit ? parseInt(limit) : 20;
@@ -12,6 +12,28 @@ router.get('/',(req,res) =>{
         return res.json({
             code:200,
             recipes
+        });
+    })
+
+    .catch(err => {
+        console.log('error ',err);
+
+        return res.json({
+            code:500,
+            message:'an error occured'
+        });
+    });
+});
+//get single recipe
+router.get('/:recipeHandle',(req,res) =>{
+    let {recipeHandle} = req.params;
+
+    return Recipe.find({handle:recipeHandle})
+
+    .then(recipe => {
+        return res.json({
+            code:200,
+            recipe
         });
     })
 
