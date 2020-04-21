40c2ec1563625081824e68151bc1a1122dd732a8
Michael Peterson <michaeljoepeterson@gmail.com>
2020-04-20 19:35:31 -0600

    update to fix ingredient id not saving

--- models/recipes.js
+++ models/recipes.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const ingredientSchema = mongoose.Schema({
     ingredient: {type:String,required:true},
     amount: {type:String,required:true},
-    units: {type:String,required:true}
+    units: {type:String,required:true},
+    id:{type:Number}
 },{_id:false});
 
 const recipeSchema = mongoose.Schema({
